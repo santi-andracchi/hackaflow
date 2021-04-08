0854298ee21fa3d636dda7f36cdf660bdfc80a1d
Martin Costa <marcosta@teco.com.ar>
2021-04-08 16:53:08 -0300

    added error colors on validation fragment

--- app/src/main/java/com/example/hackaflow/validationCode/ValidationCodeFragment.kt
+++ app/src/main/java/com/example/hackaflow/validationCode/ValidationCodeFragment.kt
@@ -7,14 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.navArgs
-import com.example.hackaflow.Login.LoginFragmentDirections
 import com.example.hackaflow.R
+import com.example.hackaflow.data.UIState
+import com.example.hackaflow.extensions.toast
 import kotlinx.android.synthetic.main.fragment_validation.*
 import org.koin.android.viewmodel.ext.android.viewModel
-
 
 class ValidationCodeFragment: Fragment() {
 
@@ -27,12 +27,13 @@ class ValidationCodeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListeners()
+        setObserver()
     }
 
     private fun setListeners(){
         pinCodeEditText.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(cs: CharSequence, arg1: Int, arg2: Int, arg3: Int) {
-                if(cs.length == 5) viewModel.validateCode(cs)
+                if(cs.length == 5) viewModel.validateCode(cs) else setNormalColors()
             }
             override fun beforeTextChanged(s: CharSequence, arg1: Int, arg2: Int, arg3: Int) {}
             override fun afterTextChanged(arg0: Editable) {
@@ -40,8 +41,38 @@ class ValidationCodeFragment: Fragment() {
         })
 
         scanCodeButton.setOnClickListener {
-            val action = ValidationCodeFragmentDirections.actionValidationCodeFragmentToResultFragment(isSuccess = true)
-            requireActivity().findNavController(R.id.nav_host_fragment).navigate(action)
+
         }
+    }
+
+    private fun setObserver(){
+
+        viewModel.validationState.observe(viewLifecycleOwner, {
+            when (it) {
+                is UIState.ErrorMessage ->{
+                    toast(it.data, Toast.LENGTH_LONG)
+                    setErrorColors()
+                }
+
+                is UIState.Success -> {
+                    val action = ValidationCodeFragmentDirections.actionValidationCodeFragmentToResultFragment(isSuccess = true)
+                    requireActivity().findNavController(R.id.nav_host_fragment).navigate(action)
+                }
+                is UIState.Error -> {
+                    toast(resources.getString(R.string.connection_error), Toast.LENGTH_LONG)
+                    setErrorColors()
+                }
+            }
+        })
+    }
+
+    private fun setErrorColors(){
+        pinCodeEditText.setLineColor(ContextCompat.getColor(requireContext(), R.color.error))
+        pinCodeEditText.setTextColor(ContextCompat.getColor(requireContext(), R.color.error))
+    }
+
+    private fun setNormalColors(){
+        pinCodeEditText.setLineColor(ContextCompat.getColor(requireContext(), R.color.white))
+        pinCodeEditText.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
     }
 }
