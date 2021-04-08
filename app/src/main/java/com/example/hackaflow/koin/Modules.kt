d43c742b95a1a6849c524b51a3e360752a533bde
Axel Bautista <abautista@intermediait.com>
2021-04-08 14:02:15 -0300

    Added retrofit

--- app/src/main/java/com/example/hackaflow/koin/Modules.kt
+++ app/src/main/java/com/example/hackaflow/koin/Modules.kt
@@ -2,12 +2,38 @@ package com.example.hackaflow.koin
 
 import com.example.hackaflow.base.BaseViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 @ExperimentalCoroutinesApi
 val appModules = module {
-    viewModel { BaseViewModel() }
+    single {
+        provideRetrofit(get(), "https://flow.com.ar")
     }
+    factory { providesOkHttpClient(get()) }
+
+
+    viewModel { BaseViewModel() }
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+fun providesOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+    return OkHttpClient.Builder()
+        .connectTimeout(60L, TimeUnit.SECONDS)
+        .readTimeout(60L, TimeUnit.SECONDS)
+        .addInterceptor(interceptor)
+        .build()
+}
 
